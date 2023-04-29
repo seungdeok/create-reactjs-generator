3a83cd380dd3613941a6d84d327e937435ec7bb1
정승덕 <akk9132@naver.com>
2023-04-29 19:07:30 +0900

    chore: Add package.json

---
+++ bin/createGenerator.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+if (process.argv.length < 3) {
+  console.log("You have to provide a name to your app.");
+  console.log("For example :");
+  console.log("  npx create-reactjs-generator my-app");
+  process.exit(1);
+}
+
+function init() {}
+
+export { init };
