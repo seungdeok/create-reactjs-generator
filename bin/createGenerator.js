8f127411acfcfeb7f039187308a4d663ea337a8e
정승덕 <akk9132@naver.com>
2023-05-01 00:22:25 +0900

    style: Add prettier & editorconfig

--- bin/createGenerator.js
+++ bin/createGenerator.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-const { execSync } = require("child_process");
-const path = require("path");
-const fs = require("fs");
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
 
 function init() {
   const startTime = new Date().getTime();
   let useYarn = false;
 
   // Package manager
-  if (process.argv[0] === "yarn") {
-    console.log("Using yarn");
+  if (process.argv[0] === 'yarn') {
+    console.log('Using yarn');
     useYarn = true;
-  } else if (process.argv[0] === "npx") {
-    console.log("Using npx");
+  } else if (process.argv[0] === 'npx') {
+    console.log('Using npx');
     useYarn = false;
   } else {
-    console.log("Unknown command");
+    console.log('Unknown command');
     process.exit(1);
   }
 
@@ -25,12 +25,12 @@ function init() {
     (useYarn && process.argv.length < 4) ||
     (!useYarn && process.argv.length < 3)
   ) {
-    console.log("You have to provide a name to your app.");
-    console.log("For example :");
+    console.log('You have to provide a name to your app.');
+    console.log('For example :');
     if (useYarn) {
-      console.log("  yarn create create-reactjs-generator my-app");
+      console.log('  yarn create create-reactjs-generator my-app');
     } else {
-      console.log("  npx create-reactjs-generator my-app");
+      console.log('  npx create-reactjs-generator my-app');
     }
     process.exit(1);
   }
@@ -38,16 +38,16 @@ function init() {
   const projectName = useYarn ? process.argv[3] : process.argv[2];
   const currentPath = process.cwd();
   const projectPath = path.join(currentPath, projectName);
-  const baseGitRepo = "https://github.com/seungdeok/react-templates.git";
+  const baseGitRepo = 'https://github.com/seungdeok/react-templates.git';
 
   // Create project folder
   try {
     fs.mkdirSync(projectPath);
   } catch (err) {
-    if (err.code === "EEXIST") {
+    if (err.code === 'EEXIST') {
       console.log(projectName);
       console.log(
-        `The file ${projectName} already exist in the current directory`
+        `The file ${projectName} already exist in the current directory`,
       );
     } else {
       console.log(error);
@@ -57,7 +57,7 @@ function init() {
 
   // Copy template to project folder
   try {
-    console.log("Copy files...");
+    console.log('Copy files...');
     execSync(`git clone --depth=1 ${baseGitRepo} ${projectPath}`);
     process.chdir(projectPath);
   } catch (error) {
@@ -66,25 +66,25 @@ function init() {
   }
 
   // Remove useless files
-  const gitFolder = path.join(projectPath, ".git");
+  const gitFolder = path.join(projectPath, '.git');
   if (fs.existsSync(gitFolder)) {
     fs.rmdirSync(gitFolder, { recursive: true });
   }
 
-  const binFolder = path.join(projectPath, "bin");
+  const binFolder = path.join(projectPath, 'bin');
   if (fs.existsSync(binFolder)) {
     fs.rmdirSync(binFolder, { recursive: true });
   }
 
-  const licenseFile = path.join(projectPath, "LICENSE");
+  const licenseFile = path.join(projectPath, 'LICENSE');
   if (fs.existsSync(licenseFile)) {
     fs.rmSync(licenseFile, { recursive: true });
   }
 
   // Update the package.json file with the project name
   try {
-    console.log("Update project name");
-    const packageFile = path.join(projectPath, "package.json");
+    console.log('Update project name');
+    const packageFile = path.join(projectPath, 'package.json');
     const packageJson = JSON.parse(fs.readFileSync(packageFile));
     packageJson.name = projectName;
 
@@ -95,17 +95,17 @@ function init() {
   }
 
   // Install dependencies
-  console.log("Installing dependencies");
+  console.log('Installing dependencies');
   if (useYarn) {
-    execSync("yarn install");
+    execSync('yarn install');
   } else {
-    execSync("npm install");
+    execSync('npm install');
   }
 
   // The installation is done
   const endTime = new Date().getTime();
   console.log(`The installation is done: ${(endTime - startTime) / 1000}s`);
-  console.log("This is ready to use! 🚀");
+  console.log('This is ready to use! 🚀');
 }
 
 module.exports = { init };
--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-const { init } = require("./createGenerator");
+const { init } = require('./createGenerator');
 
 init();
